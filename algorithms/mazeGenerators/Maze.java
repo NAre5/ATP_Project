39dc5fad59839af366db3699e27c5f89aa00a5df
erant <sherlord5@gmail.com>
2018-04-16 21:34:01 +0300

    maze constructor

--- algorithms/mazeGenerators/Maze.java
+++ algorithms/mazeGenerators/Maze.java
@@ -1,4 +1,13 @@
 package algorithms.mazeGenerators;
 
 public class Maze {
+    public Maze(int[][] maze) {
+        this.maze = maze;
+    }
+
+    public int[][] getMaze() {
+        return maze.clone();//check
+    }
+
+    private int[][] maze;
 }
